90641a3b9ef2fcdfcd2bf037bb0e981a4c8c4a25
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-07-19 20:25:50 +0200

    Added width and height property to button

--- basics/src/Components/Button.tsx
+++ basics/src/Components/Button.tsx
@@ -1,20 +1,33 @@
 import styled from 'styled-components'
  
-const StyledButton = styled.button <ButtonProps>`
-
-`
-
 export type ButtonProps = {
     label: string
     size: string
 }
 
-const Button: React.FC<ButtonProps> = ({label, size})=>
+const width = ({size}:ButtonProps )=>
+    size === 'small'? '32px': '48px';
+
+const height = ({size}: ButtonProps)=> 
+size === 'small'? '32px' : '48px';
+
+
+
+const StyledButton = styled.button<ButtonProps>((props)=>{
+return `
+align-items: center;
+width: ${width(props)};
+height: ${height(props)}
+`
+})
+
+
+const Button: React.FC<ButtonProps> = ({label, size}: ButtonProps)=>
 {
 return (
-    <StyledButton label={label} size={size}/>
+    <StyledButton label={label} size={size}>{label}</StyledButton>
 )
 
 }
 
-export default Button;
+export default Button
