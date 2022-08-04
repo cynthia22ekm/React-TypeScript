d2dfc9428974dff3b201dabe55a38afcd95129a3
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-08-04 21:19:03 +0200

    Added another component to display

--- basics/src/App.tsx
+++ basics/src/App.tsx
@@ -3,11 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '../src/Components/Button'
 import PractiseFieldArray from './Practise/PractiseFieldArray';
+import PractiseFieldArrayWithNoRef from './Practise/PractiseFieldArrayWithNoRef';
 
 function App() {
   return (
-
-      <PractiseFieldArray/>
+      <PractiseFieldArrayWithNoRef/>
 
   );
 }
