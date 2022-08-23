3b31735ad339eb81ab769fd087e4b54dfc8f58b8
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-08-23 08:24:35 +0200

    Created one more FieldArray

--- basics/src/Practise/PractiseFieldArrayWithNoRef.tsx
+++ basics/src/Practise/PractiseFieldArrayWithNoRef.tsx
@@ -1,61 +1,77 @@
-import { useForm, useFieldArray } from "react-hook-form"
-import styled from "styled-components"
-import Button from "../Components/Button"
-import { yupResolver } from '@hookform/resolvers/yup'
-import * as yup from 'yup'
-import { useCallback } from "react"
+import { useForm, useFieldArray } from "react-hook-form";
+import styled from "styled-components";
+import Button from "../Components/Button";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useCallback } from "react";
 
 type FormValues = {
-
-    firstName: string
-    lastName: string
-    age: number
-    gender: string 
-}
-
+  firstName: string;
+  lastName: string;
+  age: number;
+  gender: string;
+};
 
 const StyledInput = styled.input`
-margin: 10px;
-padding: 10px;
-`
+  margin: 10px;
+  padding: 10px;
+`;
 
-const newUser = ()=> ({firstName: '',lastName: '',age:0,gender:''})
+const newUser = () => ({ firstName: "", lastName: "", age: 0, gender: "" });
 
-const genders = [{gender: 'male'}, {gender: 'female'}]
-const schema = yup.object().shape({ firstName: yup.string().required('FirstName is Required'), lastName: yup.string().required()})
+const genders = [{ gender: "male" }, { gender: "female" }];
+const schema = yup.object().shape({
+  firstName: yup.string().required("FirstName is Required"),
+  lastName: yup.string().required(),
+});
 
-const PractiseFieldArrayWithNoRef: React.FC = ()=> {
+const PractiseFieldArrayWithNoRef: React.FC = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    control,
+  } = useForm<{ registration: FormValues[] }>({
+    defaultValues: {
+      registration: [{ firstName: "", lastName: "", age: 0, gender: "" }],
+    },
+    resolver: yupResolver(schema),
+  });
 
-const {register, formState:{errors}, handleSubmit, control} = useForm<{registration: FormValues[]}>({defaultValues: {registration: [{firstName: '', lastName: '',age:0, gender:''}]}, resolver: yupResolver(schema)})
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "registration",
+  });
 
-const {fields, append, remove} = useFieldArray({control, name: 'registration'})
+  const submitHandler = useCallback(
+    (values: { registration: FormValues[] }) => {
+      console.log(values.registration);
+    },
+    []
+  );
+  return (
+    <form onSubmit={handleSubmit(submitHandler)}>
+      {fields.map((field, index) => (
+        <div key={field.id}>
+          <StyledInput {...register(`registration.${index}.firstName`)} />
+          <StyledInput {...register(`registration.${index}.lastName`)} />
+          <StyledInput {...register(`registration.${index}.age`)} />
+          <select>
+            {genders.map((gender, index) => (
+              <option value={gender.gender}>{gender.gender}</option>
+            ))}
+          </select>
+        </div>
+      ))}
+      <Button
+        size="small"
+        label="Append"
+        onClick={() => append(newUser())}
+        palette="primary"
+      />
+      <Button size="small" label="Save" type="submit" palette="primary" />
+    </form>
+  );
+};
 
-const submitHandler = useCallback((values: {registration: FormValues[]})=> {
-console.log(values.registration)}, []
-)
-
-
-    return (
-        <form onSubmit={handleSubmit(submitHandler)}>
-            { fields.map((field, index) =>(
-                <div key={field.id}>
-<StyledInput {...register(`registration.${index}.firstName`)} />
-<StyledInput {...register(`registration.${index}.lastName`)}/>
-<StyledInput {...register(`registration.${index}.age`)}/>
-<select>
-    {genders.map((gender,index) => 
-
-<option value={gender.gender}>{gender.gender}</option>
-    )}
-   
-</select>
- </div>
-
-           )) }
-<Button size='small' label='Append' onClick={()=> append(newUser())} palette='primary'/>
-<Button size='small' label='Save' type='submit' palette='primary'/>
-        </form>
-    )
-}
-
-export default PractiseFieldArrayWithNoRef
+export default PractiseFieldArrayWithNoRef;
