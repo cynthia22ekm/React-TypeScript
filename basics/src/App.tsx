4bf8c7dc644e9dfe195abd60d9342c9129de8924
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-08-28 19:01:49 +0200

    Adding textinput to app

--- basics/src/App.tsx
+++ basics/src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from '../src/Components/Button'
-import PractiseFieldArray from './Practise/PractiseFieldArray';
-import PractiseFieldArrayWithNoRef from './Practise/PractiseFieldArrayWithNoRef';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "../src/Components/Button";
+import PractiseFieldArray from "./Practise/PractiseFieldArray";
+import PractiseFieldArrayWithNoRef from "./Practise/PractiseFieldArrayWithNoRef";
+import TextInput from "./Components/TextInput";
+import { Info } from "./Components/SVG";
 
 function App() {
   return (
-      <PractiseFieldArrayWithNoRef/>
-
+    <TextInput iconVectorLeft={Info} type={"text"} placeholder={"Enter Name"} />
   );
 }
 
