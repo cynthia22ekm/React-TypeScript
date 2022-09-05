248186b50eb6d9b4da981697b6667e0a1d227146
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-09-05 15:01:21 +0200

    Created AllComponents to display all components

---
+++ basics/src/Components/AllComponents.tsx
@@ -0,0 +1,73 @@
+import Button from "./Button";
+import PractiseFieldArray from "../Practise/PractiseFieldArray";
+import PractiseFieldArrayWithNoRef from "../Practise/PractiseFieldArrayWithNoRef";
+import TextInput from "./TextInput";
+import { Ball } from "./SVG";
+import Icon from "./Icon";
+import DropDown from "./Dropdown/DropDown";
+import styled from "styled-components";
+import RadioButton from "./RadioButton/RadioButton";
+
+const StyledHeading = styled.div`
+  margin-left: 550px;
+  padding: 20px;
+`;
+
+const AllComponents: React.FC = () => {
+  const items = [
+    { name: "India" },
+    { name: "Germany" },
+    { name: "USA" },
+    { name: "UK" },
+  ];
+
+  return (
+    <div>
+      <StyledHeading>The Complete Components</StyledHeading>
+      <TextInput
+        iconVectorLeft={Ball}
+        type={"text"}
+        placeholder={"TextInput with Right and Left Icon"}
+        clearable={true}
+        iconVectorRight={Ball}
+        value={""}
+      />
+      <br />
+      <TextInput type={"text"} placeholder={"Enter Name"} />
+      <br />
+      <div>Passowrd TextInput</div>
+      <TextInput
+        iconVectorLeft={Ball}
+        type={"password"}
+        placeholder={"Enter Name"}
+        clearable={true}
+        iconVectorRight={Ball}
+      />
+      <br />
+      <TextInput
+        iconVectorLeft={Ball}
+        type={"text"}
+        placeholder={"Text Input with Error Message"}
+        clearable={true}
+        iconVectorRight={Ball}
+        value={""}
+        errorMessage={"This value is incorrect"}
+        hasErrors={true}
+      />
+      <br />
+      <Button label={"Button Component"} size={"large"} palette={"default"} />
+      <br />
+      <br />
+      <Icon vector={Ball} />
+      <br />
+      <br />
+      <DropDown title={"Please select a Country"} items={items} />
+      <br />
+      <br />
+
+      {<RadioButton items={items} />}
+    </div>
+  );
+};
+
+export default AllComponents;
