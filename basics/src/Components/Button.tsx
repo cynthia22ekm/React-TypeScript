78a6693e7724e63e741d68532b1eeda4810dd269
cynthia22ekm <cynthiajoseph26291@gmail.com>
2022-09-05 11:53:05 +0200

    Changed the width of large button

--- basics/src/Components/Button.tsx
+++ basics/src/Components/Button.tsx
@@ -1,47 +1,59 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export type ButtonSize = 'small'|'large'
-export type ButtonPalette = 'danger'| 'dark' | 'default' | 'primary'
-export type ButtonType = 'submit' | 'button'
- 
+export type ButtonSize = "small" | "large";
+export type ButtonPalette = "danger" | "dark" | "default" | "primary";
+export type ButtonType = "submit" | "button";
+
 export type ButtonProps = {
-    label: string
-    type?: ButtonType
-    size: ButtonSize
-    palette: ButtonPalette
-    onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
-}
+  label: string;
+  type?: ButtonType;
+  size: ButtonSize;
+  palette: ButtonPalette;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+};
 
-const width = ({size}:ButtonProps )=>
-    size === 'small'? '72px': '68px';
+const width = ({ size }: ButtonProps) => (size === "small" ? "72px" : "100px");
 
-const height = ({size}: ButtonProps)=> 
-size === 'small'? '30px' : '40px';
+const height = ({ size }: ButtonProps) => (size === "small" ? "30px" : "40px");
 
-const background = ({palette} : ButtonProps)=>
+const background = ({ palette }: ButtonProps) =>
+  palette === "danger"
+    ? "red"
+    : palette === "dark"
+    ? "grey"
+    : palette === "default"
+    ? "white"
+    : "lightblue";
 
-    palette === 'danger' ? 'red' : palette === 'dark'? 'grey':palette === 'default'? 'white' : 'lightblue';
-
-
-
-const StyledButton = styled.button<ButtonProps>((props)=>{
-return `
+const StyledButton = styled.button<ButtonProps>((props) => {
+  return `
 display: in;ine-flex;
 align-items: center; 
 justify-content: center;
 width: ${width(props)};
 height: ${height(props)};
 background: ${background(props)};
-`
-})
+`;
+});
 
+const Button: React.FC<ButtonProps> = ({
+  label,
+  size,
+  palette,
+  type = "button",
+  onClick,
+}: ButtonProps) => {
+  return (
+    <StyledButton
+      label={label}
+      size={size}
+      type={type}
+      palette={palette}
+      onClick={onClick}
+    >
+      {label}{" "}
+    </StyledButton>
+  );
+};
 
-const Button: React.FC<ButtonProps> = ({label, size,palette, type='button', onClick}: ButtonProps)=>
-{
-return (
-    <StyledButton label={label} size={size} type={type}  palette={palette} onClick={onClick}>{label} </StyledButton>
-)
-
-}
-
-export default Button
+export default Button;
